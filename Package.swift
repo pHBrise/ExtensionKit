203065ed959db6b71e9bf9587231b22791a4d010
Phanvit <phanvit@rs.co.th>
2023-05-10 17:23:45 +0700

    fix code

--- Package.swift
+++ Package.swift
@@ -5,9 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ExtensionKit",
-    platforms: [
-        .iOS(.v14)
-    ],
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
