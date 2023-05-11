017f159f2c57208183d4dea29acff917cdf841be
Phanvit <phanvit@rs.co.th>
2023-05-11 12:57:19 +0700

    [fix]

--- Sources/ExtensionKit/RoundViewExtension.swift
+++ Sources/ExtensionKit/RoundViewExtension.swift
@@ -70,9 +70,9 @@ import UIKit
     }
 }
 
-@IBDesignable class RoundedView: UIView {
+@IBDesignable open class RoundedView: UIView {
     
-    override func awakeFromNib() {
+    open override func awakeFromNib() {
         super.awakeFromNib()
         if shadow {
             applyShadow()
@@ -81,7 +81,7 @@ import UIKit
         applyCornerRadius()
     }
     
-    override func layoutSubviews() {
+    open override func layoutSubviews() {
         super.layoutSubviews()
         if shadow {
             applyShadow()
@@ -147,7 +147,7 @@ import UIKit
             layer.borderWidth = CGFloat(borderWidth)
         }
     }
-    override class func prepareForInterfaceBuilder() {
+    open override class func prepareForInterfaceBuilder() {
         super.prepareForInterfaceBuilder()
     }
 }
